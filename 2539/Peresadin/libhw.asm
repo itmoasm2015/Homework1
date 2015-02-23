8cbc3a7f712396f0a461b841bdce44624571593e
Ilya Peresadin <pva701@gmail.com>
2015-02-23 18:31:11 +0300

    impelemented itoa. not tested. not cdecl

--- 2539/Peresadin/libhw.asm
+++ 2539/Peresadin/libhw.asm
@@ -4,7 +4,8 @@ section .text
 
 FLAG_SIGN_BIT equ 1<<0
 TYPE_BIT equ 1<<1
-SIGN_MASK equ 1<<7
+SIZE_BIT equ 1<<2
+SIGN_BIT equ 1<<7
 
 ;
 ;
@@ -12,32 +13,55 @@ itoa:
     ret
 
 hw_sprintf:
-    mov cl, [esp]
-    cmp cl, [SIGN_MASK]
-    xor ecx, ecx
+    xor cl, cl
+    mov [ebx], cl
 
-    jnae .minus_sign
-    jmp  .check_flag_sign
+    test al, [TYPE_BIT]
+    jnz .sign_done;if %u - without sign
+
+    mov cl, [esp]
+    cmp cl, [SIGN_BIT]
+    mov ecx, 0
+    jnae .minus_sign;if number is negative
+    jmp  .check_flag_sign;else
 
     .minus_sign
         mov cl, '-'
-        mov [ebx], cl
-        mov cl, [SIGN_MASK]
-        xor [ebx], cl
-        xor ecx, ecx
+        mov [ebx], cl;store sign
+        mov cl, [SIGN_BIT]
+        xor [ebx], cl;unset sign bit
         jmp .sign_done
 
     .check_flag_sign
-        ;//TODO write
-    
+        test al, [SIGN_BIT]
+        jz .sign_done;flag + doesn't set
+        mov cl, '+'
+        mov [ebx], cl
+        jmp .sign_done
+
     .sign_done
-    inc ecx
+    mov ecx, 1
     mov esi, 10
 
-    mov dword edx, [esp]
-    add esp, 4
-    mov dword eax, [esp]
-    add esp, 4
+    test al, [SIZE_BIT]
+    jz .is32bit_number;if number is 32 bit
+    jmp .is64bit_number
+
+    .is32bit_number
+        xor edx, edx
+        mov eax, [esp]
+        add esp, 4
+        jmp .size_done
+
+    .is64bit_number
+        mov edx, [esp]
+        add esp, 4
+        mov eax, [esp]
+        add esp, 4
+        jmp .size_done
+
+    .size_done
+
     .loop
         mov ebp, edx;ebp - storage of edx (r8)
         mov edi, eax;edi - storage of eax (r9)
