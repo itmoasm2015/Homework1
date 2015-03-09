5d52b9ff052bf86290adc6231042595b1eb7d77d
NoNick <aleksandrs634@gmail.com>
2015-03-09 21:10:27 +0000

    done

--- 2537/Khimulya/hw_sprintf.asm
+++ 2537/Khimulya/hw_sprintf.asm
@@ -1,22 +1,22 @@
 global hw_sprintf
 
 section .text
-
     ; flags for processing number
     ; should current symbol be processed as a part of number format sequence
-    FORMAT_SEQUENCE    equ    1
+    FLAG_CTRL_SEQ    equ    1
     ; add plus symbol to non-negative numbers
-    PLACE_PLUS        equ    1 << 2
+    FLAG_PLUS        equ    1 << 2
     ; place space at first position if there's no sign
-    PLACE_SPACE        equ    1 << 3
+    FLAG_SPACE        equ    1 << 3
     ; align number to left side
-    ALIGN_LEFT     equ        1 << 4
+    FLAG_ALIGN_LEFT     equ        1 << 4
     ; extend number to required size by adding zeros
-    FILL_ZEROS         equ    1 << 5
+    FLAG_ZEROS         equ    1 << 5
     ; does number take 64 bit?
     FLAG_LONG          equ    1 << 6
     ; does number in two's complinet
     FLAG_UNSIGNED      equ    1 << 7
+    ; following flags are internal
     ; have flags read?
     FLAGS_ENDED        equ    1 << 8
     ; should we fill string to reach minimal number width?
@@ -27,13 +27,15 @@ section .text
     SPACE              equ    1 << 12
     ; if one symbol was written directly to out
     DEC_COUNT          equ    1 << 13
+    ; if cf is true when inc lowest part of long
+    CARRY              equ    1 << 14
 
-    ; converts number to string
+    ; write number to string
     ;
     ; edi is corrupted at the time of return
     ;
     ; @param eax stores the flags
-    ; @param edi the number to be converted
+    ; @param edi is the number to be converted or address of long if FLAG_LONG is set
     ; @param ebx pointer to char* for string
     ; @param esi number's width, 0 if not set
     itoa:
@@ -41,18 +43,47 @@ section .text
         push ecx
         push eax
         mov ecx, esp ; end of number string in stack
-
         test eax, FLAG_UNSIGNED
         jnz .add_plus
-        test edi, 0x80000000      ; is negative?
-        jnz .add_minus
+        test eax, FLAG_LONG        ; is negative?
+        jz .int_neg_check
+        push edi
+        mov edi, [edi + 4]         ; highest 32 bits
+        test edi, 0x80000000
+        jnz .from2compl_long
+        jmp .add_plus_long
+    .int_neg_check:
+        test edi, 0x80000000
+        jnz .from2compl_int
         jmp .add_plus              ; checks flags & adds plus if has to
-    ; depends on FILL_ZEROS flag we should write sign as the first symbol of string
-    ; or as the symbol before the first significant figure
-    .add_minus:
+    ; depends on FLAG_ZEROS flag we should write sign as the first symbol of string
+    ; or as the symbol before the first significant figure (i.e. last push in stack)
+    .from2compl_long:
+        mov edi, [esp]
+        push ebx
+        mov edi, [edi]             ; lowest 32 bits
         not edi
         inc edi
-        test eax, FILL_ZEROS
+        jnc .no_carry
+        or eax, CARRY
+    .no_carry:
+        mov ebx, [esp + 4]
+        mov [ebx], edi
+        mov edi, [ebx + 4]         ; highest 32 bits
+        not edi
+        test eax, CARRY
+        jz .no_carry_again
+        inc edi
+    .no_carry_again:
+        mov [ebx + 4], edi
+        pop ebx
+        pop edi
+        jmp .add_minus
+    .from2compl_int:
+        not edi
+        inc edi
+    .add_minus:
+        test eax, FLAG_ZEROS
         jz .add_minus_flag
         mov byte [ebx], '-'
         inc ebx
@@ -61,10 +92,12 @@ section .text
     .add_minus_flag:
         or eax, MINUS
         jmp .to_decimal_str
+    .add_plus_long:
+        pop edi
     .add_plus:
-        test eax, PLACE_PLUS
+        test eax, FLAG_PLUS
         jz .add_space
-        test eax, FILL_ZEROS
+        test eax, FLAG_ZEROS
         jz .add_plus_flag
         mov byte [ebx], '+'
         inc ebx
@@ -74,33 +107,29 @@ section .text
         or eax, PLUS
         jmp .to_decimal_str
     .add_space:
-        test eax, PLACE_SPACE
+        test eax, FLAG_SPACE
         jz .to_decimal_str
         or eax, SPACE
         jmp .to_decimal_str
     ; write decimal string to stack
     .to_decimal_str:
         mov [ecx], eax
+        mov edx, eax    ; temporary place for flags
         mov eax, edi
         mov edi, 10
-        dec esp
-        .loop:
-            xor edx, edx
-            div edi
-            add edx, '0'
-            mov byte [esp], dl
-            dec esp
-            cmp eax, 0
-            jne .loop
+        test edx, FLAG_LONG
+        jnz .push_long
+        jmp .push_int
+    .push_done:
         mov eax, [ecx]
         test eax, PLUS
-        jnz .place_plus
+        jnz .FLAG_PLUS
         test eax, MINUS
         jnz .place_minus
         test eax, SPACE
-        jnz .place_space
+        jnz .FLAG_SPACE
         jmp .done_placing
-    .place_plus:
+    .FLAG_PLUS:
         mov byte [esp], '+'
         dec esp
         jmp .done_placing
@@ -108,7 +137,7 @@ section .text
         mov byte [esp], '-'
         dec esp
         jmp .done_placing
-    .place_space:
+    .FLAG_SPACE:
         mov byte [esp], ' '
         dec esp
         jmp .done_placing
@@ -129,10 +158,10 @@ section .text
         dec esi
         jz .write
     .continue:
-        test eax, ALIGN_LEFT
+        test eax, FLAG_ALIGN_LEFT
         jnz .write
         mov edx, ' '
-        test eax, FILL_ZEROS
+        test eax, FLAG_ZEROS
         jz .call_fill
         mov edx, '0'
         mov eax, [ecx]
@@ -151,7 +180,7 @@ section .text
             jne .loop1
         add esp, 4      ; erase flags in stack
 
-        test eax, ALIGN_LEFT
+        test eax, FLAG_ALIGN_LEFT
         jz .done_itoa
         test eax, FILL
         jz .done_itoa
@@ -163,6 +192,60 @@ section .text
         pop ecx
         pop edx
         ret
+
+    ; converts 32-bit number in characters and places them in stack
+    ; so first symbol in stack is first digit of number
+    ; don't declare as a function because call spoils all the stack magic
+    ;
+    ; corrupts edx
+    ; corrupts eax
+    ; decrease esp
+    ;
+    ; @param eax number to be converted
+    ; @param edi base of numeral system
+    .push_int:
+        dec esp
+        .loop:
+            xor edx, edx
+            div edi
+            add edx, '0'
+            mov byte [esp], dl
+            dec esp
+            cmp eax, 0
+            jne .loop
+        jmp .push_done
+
+    ; same as .push_int but for 64-bit number, also eax is address
+    ;
+    ; corrupts edx
+    ; corrupts eax
+    ; corrupts edi
+    ; decrease esp
+    ;
+    ; @param eax address to number to be converted
+    ; @param edi base of numeral system
+    ; @param ebx out string address
+    .push_long:
+        mov [ebx], edi        ; we'll use eax and edi as parts of number
+                              ; and divisor will be in memory
+        mov edi, [eax]        ; lowest 32 bits = l
+        mov eax, [eax + 4]    ; highest 32 bits = h
+        dec esp
+        .loop4:
+            xor edx, edx
+            div dword [ebx]   ; edx = h % 10
+            xchg edi, eax     ; edx:eax = (h % 10) * 2 ** 32 + l
+            div dword [ebx]
+            add edx, '0'
+            mov byte [esp], dl
+            dec esp
+            xchg eax, edi
+            cmp edi, 0
+            jnz .loop4
+            cmp eax, 0
+            jnz .loop4
+        mov [ebx], dword 0
+        jmp .push_done
 
     ; places esi symbols with code edx in out string, esi > 0
     ;
@@ -198,8 +281,8 @@ section .text
         mov ecx, [esp + 24]    ; format
         lea edi, [esp + 28]
         xor eax, eax           ; flags register
-        xor edx, edx
     .process_next:
+        xor edx, edx
         mov dl, byte [ecx]
         cmp edx, 0
         je .done
@@ -240,20 +323,26 @@ section .text
     .print_long:
         ; make sure there's second 'l'
         inc ecx
-        mov edx, [ecx]
+        mov dl, byte [ecx]
         cmp edx, 'l'
         jne .incorrect_sequence
         or eax, FLAG_LONG
-        ; TODO
-        xor eax, eax
         inc ecx
-        jmp .process_next
+        jmp .process_format
     .print_int:
-        add  esp, 4           ; erase address of format sequence start, no need anymore
+        add esp, 4           ; erase address of format sequence start, no need anymore
         push edi
+        test eax, FLAG_LONG
+        jnz .send_address
+      .send_value:
         mov edi, [edi]
+      .send_address:
         call itoa
         pop edi
+        test eax, FLAG_LONG
+        jz .add_4
+        add edi, 4
+      .add_4:
         add edi, 4
         xor eax, eax
         inc ecx
@@ -267,34 +356,33 @@ section .text
     .plus:
         test eax, FLAGS_ENDED
         jnz .incorrect_sequence
-        or eax, PLACE_PLUS
+        or eax, FLAG_PLUS
         inc ecx
         jmp .process_format
     .minus:
         test eax, FLAGS_ENDED
         jnz .incorrect_sequence
-        or eax, ALIGN_LEFT
+        or eax, FLAG_ALIGN_LEFT
         inc ecx
         jmp .process_format
     .space:
         test eax, FLAGS_ENDED
         jnz .incorrect_sequence
-        or eax, PLACE_SPACE
+        or eax, FLAG_SPACE
         inc ecx
         jmp .process_format
     .zero:
         test eax, FLAGS_ENDED
         jnz .digit
-        or eax, FILL_ZEROS
+        or eax, FLAG_ZEROS
         inc ecx
         jmp .process_format
-
     .percent:
-        test eax, FORMAT_SEQUENCE
+        test eax, FLAG_CTRL_SEQ
         jnz .incorrect_sequence   ; '%' in formatting sequence
         xor eax, eax
         xor esi, esi
-        or eax, FORMAT_SEQUENCE
+        or eax, FLAG_CTRL_SEQ
         push ecx                  ; start of format sequence is now in stack
         inc ecx
         mov dl, byte [ecx]
@@ -310,7 +398,6 @@ section .text
     .incorrect_sequence:
         xor eax, eax
         pop esi                   ; start of incorrect sequence
-
         .loop2:
             mov dl, byte [esi]
             mov [ebx], dl
@@ -322,7 +409,6 @@ section .text
 
     .done:
         mov [ebx], byte 0         ; end of string
-        lea esp, [ebp - 0x3C]     ; place in the stack before 4 first pushes
         pop ebx
         pop edi
         pop esi
