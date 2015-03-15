ef63ebe3676d370a45ab82b5b2d834c4da3011f5
heat_wave <allpicksucks@gmail.com>
2015-03-15 15:47:57 +0300

    Correct processing of malformed commands, more code, more comments, and I HATE DEBUG.

--- 2537/Malimonov/hw_sprintf.asm
+++ 2537/Malimonov/hw_sprintf.asm
@@ -1,7 +1,18 @@
 global hw_sprintf
 
-MINUS       equ '-'
-ASCII_CONST equ 48
+%define PLUS_FLAG 1
+%define ALIGN_LEFT_FLAG 1 << 1
+%define SPACE_FLAG 1 << 2
+%define LONG_LONG_FLAG 1 << 3
+%define ZERO_FLAG 1 << 4
+%define UNSIGNED_FLAG 1 << 5
+%define NEGATIVE_FLAG 1 << 6
+
+%define MINUS '-'
+%define ASCII_CONST 48
+
+%define set_flag(f) or ebx, f
+%define test_flag(f) test ebx, f
 
 section .text
 
@@ -18,28 +29,131 @@ hw_sprintf:                     ; save the status of the registers
     lea ebp, [esp + 32 - 4]     ; arguments
 
 .process:                       ; process characters one at a time
-    mov al, byte [esi]
-    cmp al, '%'                 ; check if we met a special character
+    cmp byte[esi], '%'          ; check if we met a special character
     je .format
-    mov [edi], al               ; put the character into out
-    inc edi
+    movsb
+    cmp byte[esi], 0            ; put the character into out
+    je .return
+    jne .process
 
-.check_null:                    ; check if we met the end of the string
-    inc esi          
-    test al, al                 
-    je .return                  ; finish processing the string          
-    jmp .process                ; process another character
+.format:                        ; store esi state in case format is incorrect
+    push esi                    ; and null out the registers
+    xor ebx, ebx
+    xor edx, edx
+    xor eax, eax
+    inc esi
 
-.format:
-    cmp al, 'd'                 ; if the number is decimal
-    je .decimal
+.parse_flags:
+    lodsb                       ; load a byte into al and set corresponding flags
+
+    cmp al, '+'
+    je .set_plus_flag
+
+    cmp al, '-'
+    je .set_minus_flag
+
+    cmp al, ' '
+    je .set_space_flag
+
+    cmp al, '0'
+    je .set_zero_flag
+
+.get_width:                     ; if the character is not a digit, skip this step
+    cmp al, '0'
+    jl .break_width_loop
+    cmp al, '9'
+    jg .break_width_loop
+
+    imul edx, 10                ; calculate the width here
+    sub eax, ASCII_CONST
+    add edx, eax
+
+    lodsb
+    jmp .get_width
+
+.break_width_loop:
+    ;push edx
+
+.get_size:
+    dec esi                     ; fix an extra lodsb instruction moving the pointer forward
+    cmp word[esi], 'll'         ; check if we met a long long specifier
+    jne .get_type
+
+    add esi, 2                  ; move the pointer away from 'll' specifier
+    set_flag(LONG_LONG_FLAG)    ; and set the corresponding flag
+
+.get_type:                      ; parse the type symbol
+    lodsb
+
+    cmp al, 'u'
+    je .set_unsigned_flag
+    cmp al, 'i'
+    je .format_parse_finished
+    cmp al, 'd'
+    je .format_parse_finished
+    cmp al, '%'
+    je .print_percent
+
+    jmp .malformed_format
+
+.set_unsigned_flag:             ; actually set the flags
+    set_flag(UNSIGNED_FLAG)
+    jmp .format_parse_finished
+
+.set_plus_flag:
+    set_flag(PLUS_FLAG)
+    jmp .parse_flags
+
+.set_minus_flag:
+    set_flag(ALIGN_LEFT_FLAG)
+    jmp .parse_flags
+
+.set_space_flag:
+    set_flag(SPACE_FLAG)
+    jmp .parse_flags
+
+.set_zero_flag:
+    set_flag(ZERO_FLAG)
+    jmp .parse_flags
+
+.format_parse_finished:
+    pop eax
+    jmp .output
+
+.malformed_format:
+    pop esi                     ; restore position in string to the start of command sequence
+    movsb                       ; move % to out and keep processing the string
+    jmp .process
+
+.output:
+    test_flag(LONG_LONG_FLAG)   ; check if the number is int64
+    jnz .int64
+
+.int32:                         
+    xor edx, edx
+    mov eax, [ebp]
+    add dword ecx, 4
+
+    test_flag(UNSIGNED_FLAG)    ; check if the number is unsigned
+    jnz .done
+
+    cmp eax, 0
+    jl .negative_int32
+    jmp .done
+
+.negative_int32                 ; if the number is negative
+    set_flag(NEGATIVE_FLAG)     ; set the flag and flip its sign
+    neg eax
+    jmp .done
+
+.int64:
+
+.done:
+    call put_first_character    ; print the requested character before the number
 
 .decimal:
     inc esi
-    mov eax, [ebp]              ; get a number to be put into out
-    xor ecx, ecx                ; null out the counter of the number's length
-    cmp eax, 0                  ; if the number is negative,
-    jl .print_minus             ; print a minus and flip its sign
+    xor ecx, ecx                 ; null out the counter of the number's length
 
 .unsigned:
 
@@ -60,14 +174,12 @@ hw_sprintf:                     ; save the status of the registers
     dec ecx                     ; reduce the number's length
     test ecx, ecx               ; if it is not zero
     jnz .reverse_get            ; keep getting digits
-    jmp .check_null             ; else
+    jmp .process                ; else
 
-.print_minus:                   ; quite obvious
-    mov edx, MINUS
-    mov [edi], edx              ; put a minus sign into out
-    inc edi
-    neg eax                     ; flip the number's sign
-    jmp .parse_number           ; and start reading it onto the stack
+.print_percent                  ; command sequence contains the '%' character
+    add esp, 4                  ; no need to store width anymore
+    stosb                       ; move % to output, AL -> EDI
+    jmp .process
 
 .return:                        ; restore the status of the registers
     pop edi
@@ -75,3 +187,34 @@ hw_sprintf:                     ; save the status of the registers
     pop ebp
     pop ebx
     ret                         ; and finish
+
+;_______________________________
+
+put_first_character:            ; subroutine that prints requested characters
+    test_flag(NEGATIVE_FLAG)    ; before the number
+    jnz .put_minus              ; according to the flags
+
+    test_flag(PLUS_FLAG)
+    jnz .put_plus
+
+    test_flag(SPACE_FLAG)
+    jnz .put_space
+    jmp .ret
+
+.put_minus:
+    mov byte [edi], '-'
+    inc edi
+    jmp .ret
+
+.put_plus:
+    mov byte [edi], '+'
+    inc edi
+    jmp .ret
+
+.put_space:
+    mov byte [edi], ' '
+    inc edi
+    jmp .ret
+
+.ret:
+    ret
