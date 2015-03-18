240a1a3a8f612d0fc4cc89f3a3d2123421bcafa0
Eugene <jon-536@mail.ru>
2015-03-18 19:14:49 +0300

    delete debug info

--- 2538/nemchenko/hw_sprintf.asm
+++ 2538/nemchenko/hw_sprintf.asm
@@ -251,7 +251,6 @@ num_to_str:
     lea ebp, [esp + 8]
     push esi
     push ebx
-    push edi
     
     mov [len_str_repr], dword 0
     mov ecx, 10
@@ -260,7 +259,6 @@ num_to_str:
     ; ebp + 4 --> low bits
     mov edx, [ebp]
     mov eax, [ebp + 4]
-    mov edi, len_str_repr
 
     .while_num_not0:
         cmp eax, 0
@@ -286,10 +284,7 @@ num_to_str:
         jmp .while_num_not0
 
     .end_while_num_not0:
-    mov esi, str_repr
-    mov esi, [len_str_repr]
 
-    pop edi
     pop ebx
     pop esi
     pop ebp
