27b3d44e2be9d6db896028d3fa8a82cec69d4a77
Alexey Katsman <alex2007508@gmail.com>
2015-06-10 13:48:32 +0300

    Something new

--- 2538/Katsman/hw_sprintf.asm
+++ 2538/Katsman/hw_sprintf.asm
@@ -1,1 +1,33 @@
 global hw_sprintf
+
+section .text
+
+PLUS_FLAG equ 1
+SPACE_FLAG equ 2
+ALIGN_FLAG equ 4
+ZERO_FLAG equ 8
+flags equ 16
+flags equ 32
+flags equ 64
+flags equ 128
+flags equ 256
+
+hw_sprintf:
+    push ebp
+    mov ebp, esp
+    push esi
+    push eax
+    push ecx
+
+    mov eax, [ebp + 8] ; eax contains our out string
+    mov ebx, [ebp + 12] ; ebx contains format string
+    mov ecx, [ebp + 16] ; ecx contains pointer on first argument
+
+
+    
+.end:
+    pop ecx
+    pop eax
+    pop esi
+    pop ebp
+    ret
